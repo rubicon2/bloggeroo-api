150c7adf154c8fc14a99e4191bc14ff19c96a06b
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-04-14 16:06:27 +0100

    Refactor validators to make them more re-usable

--- middleware/validators.mjs
+++ middleware/validators.mjs
@@ -1,26 +1,38 @@
 import { body } from 'express-validator';
 
-function createSignUpChain() {
-  return [
-    body('email')
-      .trim()
-      .notEmpty()
-      .withMessage('Email is required')
-      .isEmail()
-      .withMessage('Email must be in correct format'),
-    body('password')
-      .notEmpty()
-      .withMessage('Password is required')
-      .isStrongPassword({ minLength: 8 })
-      .withMessage(
-        'Password must be at least 8 characters, with at least one of each: a digit, symbol, upper and lowercase characters',
-      ),
-    body('confirm_password')
-      .notEmpty()
-      .withMessage('Confirm password is required')
-      .custom((value, { req }) => value === req.body.password)
-      .withMessage('Password and confirm password do not match'),
-  ];
+function email() {
+  return body('email')
+    .trim()
+    .notEmpty()
+    .withMessage('Email is required')
+    .isEmail()
+    .withMessage('Email must be in correct format');
 }
 
-export { createSignUpChain };
+function password() {
+  return body('password')
+    .notEmpty()
+    .withMessage('Password is required')
+    .isStrongPassword({ minLength: 8 })
+    .withMessage(
+      'Password must be at least 8 characters, with at least one of each: a digit, symbol, upper and lowercase characters',
+    );
+}
+
+function confirmPassword() {
+  return body('confirm_password')
+    .notEmpty()
+    .withMessage('Confirm password is required')
+    .custom((value, { req }) => value === req.body.password)
+    .withMessage('Password and confirm password do not match');
+}
+
+function createSignUpChain() {
+  return [email(), password(), confirmPassword()];
+}
+
+function createResetPasswordChain() {
+  return [password(), confirmPassword()];
+}
+
+export { email, password, createSignUpChain, createResetPasswordChain };
