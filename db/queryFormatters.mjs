c15a5d6fa990959b8928ca245ee7a1a815971c14
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-05-12 16:21:45 +0100

    Allow client to search blogs by body text

--- db/queryFormatters.mjs
+++ db/queryFormatters.mjs
@@ -2,6 +2,7 @@ import { formatters, processors } from 'url-query-to-prisma';
 
 const blogsQueryFormatter = {
   title: formatters.where('contains', { mode: 'insensitive' }),
+  body: formatters.where('contains', { mode: 'insensitive' }),
   fromDate: formatters.groupWhere('publishedAt', 'gte', processors.date),
   toDate: formatters.groupWhere('publishedAt', 'lte', processors.date),
 };
