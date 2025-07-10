7dc8825e3e6cb894bd3853371180e4e4cea89147
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-07-10 12:51:56 +0100

    Add id to jwt on auth access route

--- controllers/auth.mjs
+++ controllers/auth.mjs
@@ -5,6 +5,7 @@ function postAccess(req, res) {
   // Use token.verify and auth.getUser - refresh token will be verified, and user put in req.user.
   const token = jwt.sign(
     {
+      id: req.user.id,
       email: req.user.email,
       isAdmin: req.user.isAdmin,
       isBanned: req.user.isBanned,
