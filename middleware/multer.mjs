2a8f8d549cbf7d3565358f032481fc1f18f22552
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2026-01-14 11:28:39 +0000

    Middleware > Multer: restrict upload file types to just jpeg, png and gif

--- middleware/multer.mjs
+++ middleware/multer.mjs
@@ -1,5 +1,11 @@
 import multer from 'multer';
 
+const allowedUploadMimeTypes = JSON.parse(
+  process.env.UPLOAD_ALLOWED_MIME_TYPES,
+);
+
+const errorMsg = process.env.UPLOAD_MIME_TYPE_ERROR;
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, process.env.VOLUME_MOUNT_PATH);
@@ -10,4 +16,12 @@ const storage = multer.diskStorage({
   },
 });
 
-export default multer({ storage });
+const fileFilter = (req, file, cb) => {
+  if (allowedUploadMimeTypes.includes(file.mimetype)) {
+    return cb(null, true);
+  } else {
+    return cb(new Error(errorMsg), false);
+  }
+};
+
+export default multer({ storage, fileFilter });
