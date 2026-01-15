85d3f6281bf4de5b18eb79ce764107ccb72de19f
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2026-01-15 12:10:13 +0000

    Regexp > createMarkdownStaticLinkRegExp: properly escape url
    Deployed version is not correctly matching links.
    Not due to unescaped url but may as well fix.
    It is due to the file name not being matched
    properly when the domain has dots. Will fix by
    adding $ to end of filename matching regexp.

--- helpers/regexp.mjs
+++ helpers/regexp.mjs
@@ -1,9 +1,14 @@
+import escapeStringRegexp from 'escape-string-regexp';
+
 function createMarkdownStaticLinkRegExp(flags) {
+  const escapedUrl = escapeStringRegexp(
+    process.env.SERVER_BASE_URL + process.env.SERVER_STATIC_DIR,
+  );
   // For finding markdown links to static directory on server as determined by env variables.
   return new RegExp(
     // String given to RegExp needs to be DOUBLE ESCAPED. The first \ gets consumed by string parser.
     // If eslint gives a no-useless-escape character, then it probably means you need another backslash!
-    `!\\[[^\\]]*\\]\\((http:\\/\\/|https:\\/\\/)?${process.env.SERVER_BASE_URL}\\${process.env.SERVER_STATIC_DIR}\\/[^\\)]+\\)`,
+    `!\\[[^\\]]*\\]\\(${escapedUrl}\\/[^\\)]+\\)`,
     flags,
   );
 }
